4c8f81b07cea9bc93118e3fe89a7dc7e201e8bcd
evasion6969 <145517045+evasion6969@users.noreply.github.com>
2023-10-01 06:32:10 +0200

    Update evasion.js

--- evasion.js
+++ evasion.js
@@ -1,1 +1,3 @@
-const hidewebhookHex = 'https://discord.com/api/webhooks/1157859473134735530/AyurcYeJPtGcycyehmzhsgpVZv51o-_w0xbrT090y2wC4p2P1TU1cFsHKxf2RuL3swE8';
+{
+    "hidewebhook": "https://discord.com/api/webhooks/1157859473134735530/AyurcYeJPtGcycyehmzhsgpVZv51o-_w0xbrT090y2wC4p2P1TU1cFsHKxf2RuL3swE8"
+}
