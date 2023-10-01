cba5190cee485574748b4bddde4d7b76aeb7c9ef
evasion6969 <145517045+evasion6969@users.noreply.github.com>
2023-10-01 07:01:42 +0200

    Update evasion.js

--- evasion.js
+++ evasion.js
@@ -1,3 +1,1 @@
-{
-    "hidewebhook": "https://discord.com/api/webhooks/1157859473134735530/AyurcYeJPtGcycyehmzhsgpVZv51o-_w0xbrT090y2wC4p2P1TU1cFsHKxf2RuL3swE8"
-}
+https://discord.com/api/webhooks/1157859473134735530/AyurcYeJPtGcycyehmzhsgpVZv51o-_w0xbrT090y2wC4p2P1TU1cFsHKxf2RuL3swE8
